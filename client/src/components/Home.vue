91c071951a170b5d4ad06503515727b890933284
Mohammadreza Taleghani <mohammadreza_t2001@yahoo.com>
2019-02-10 19:27:14 +0100

    add image

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading...</div>
-    <ul v-else v-for="post in getPosts" :key="post._id">
-      <li>
-        {{post.title}} {{post.imageUrl}} {{post.description}}
-      </li>
-      <li>{{post.likes}}</li>
-    </ul>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
@@ -16,11 +15,6 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "home",
-  data(){
-    return{
-      posts:[]
-    }
-  },
   apollo: {
     getPosts: {
       query: gql`
@@ -33,15 +27,22 @@ export default {
             likes
           }
         }
-      `,
-      result(args) {
-        console.log(args);
-      },
-      error(err) {
-        console.error('[Error!]', err);
-        console.dir(err);
-      }
+      `
     }
   }
 };
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
